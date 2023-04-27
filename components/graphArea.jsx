82f40525c729acd803fb78aa79ff4b03999bfd2e
Hsm085 <himanshu.maurya@investwellonline.com>
2023-04-27 10:59:05 +0530

    Components Added

---
+++ components/graphArea.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import StackedGraph from './stackedGraph.jsx'
+
+export default function GraphArea(props) {
+  
+  return (
+    <div className='leftDiv'>
+        <div className='vennDiagramLeftLabel'><p>SCHEME A</p></div>
+
+      
+        <div className="vennDiagramLeft">
+              <div className='sideLabel'>%  WEIGHT</div>
+              <div className='graphA'>
+                  <StackedGraph  value = {props.values.holdingAOnlyNetAsset} colorGraph={props.colorGraph} color = {"#39B0DC"}/>
+              </div> 
+              <div className='graphALabel'>IN SCHEME A NOT IN SCHEME B</div>
+
+
+
+              <div className='commonGraphA'>
+                <StackedGraph  value = {props.values.commonHoldingA} colorGraph={props.colorGraph} color = {"#39B0DC"}/>
+              </div>
+              <div className='commonGraphB'>
+                <StackedGraph  value = {props.values.commonHoldingsB} colorGraph={props.colorGraph} color = {"#F15562"}/>
+              </div>
+
+              <div className='commonGraphLabel'>COMMON STOCKS</div>
+
+        </div>
+
+        <div className='vennDiagramRightLabel'><p>SCHEME B</p></div>
+        <div className="vennDiagramRight">
+          <div className='graphB'>
+              <StackedGraph  value = {props.values.holdingBOnlyNetAsset} colorGraph={props.colorGraph} color = {"#F15562"}/>
+          </div>
+          <div className='graphBLabel'>IN SCHEME B NOT IN SCHEME A</div>
+
+        </div>
+        <span className='vennLabel'>Note : The Venn Diagram potrays portfolio Overlap between Scheme A and B. Cash is not included</span>
+  
+  </div>
+  )
+}
---
+++ components/portfolioOverlap.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import GraphArea from './graphArea.jsx'
+import DonutChart from './donutChart.jsx'
+import UpperLogo from '../public/commonStock.png'
+import LowerLogo from '../public/unCommonStock.png'
+import Image from 'next/image'
+
+export default function PortfolioOverlap(props) {
+  return (
+      <div className='portfolioOverlap'>
+            <GraphArea values={props.holdingsDetails.vennDiagram}/>
+            <div className='rightDiv'>
+              <div className='rightContainerA'><DonutChart values={props.holdingsDetails.overlapValue.overlapPercentage} /></div>
+
+              <div className='rightContainerB'>
+                  <div className='commonStockDiv'><Image src={UpperLogo} className='commonStockIcon' /></div>
+                  <span className='commonStock'>{props.holdingsDetails.overlapValue.commonHoldings}<br />
+                    <div className='text'>#common stocks</div>
+                  </span>
+              </div>
+
+
+
+              <div className='rightContainerC'>
+          
+                  <div className='unCommonStockDiv'><Image src={LowerLogo} className='unCommonStockIcon' /></div>
+                  <span className='unCommonStockA'>{props.holdingsDetails.overlapValue.unCommonHoldingsInA}<br />
+                    <div className='text'>#uncommon stocks in A</div>
+                  </span>
+                  <span className='totalStockA'>{props.holdingsDetails.overlapValue.totalHoldingsInA}<br />
+                    <div className='text'>#total stocks in A</div>
+                  </span>
+                
+              </div>
+
+
+
+              <div className='rightContainerD'>
+                  <div className='unCommonStockDiv'><Image src={LowerLogo} className='unCommonStockIcon' /></div>
+                  <span className='unCommonStockB'>{props.holdingsDetails.overlapValue.unCommonHoldingsInB}<br />
+                    <div className='text'>#uncommon stocks in B</div>
+                  </span>
+                  <span className='totalStockB'>{props.holdingsDetails.overlapValue.totalHoldingsInB}<br />
+                    <div className='text'>#total stocks in B</div>
+                  </span>
+                </div>
+          
+            </div>
+
+          </div>
+  )
+}
+
