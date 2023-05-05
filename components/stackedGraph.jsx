8c5880a982c2c00c9a6c9c7d212611a321bcf73c
bhavya-sharma-iwell <bhavya.sharma@investwellonline.com>
2023-05-05 15:38:42 +0530

    update:stacked graph

--- components/stackedGraph.jsx
+++ components/stackedGraph.jsx
@@ -1,41 +1,38 @@
-import React from 'react';
+import { Fragment } from "react";
 
-export default function StackedGraph(props) {
-    const divArray = [10,9,8,7,6,5,4,3,2,1];
-    const white = "#EBEBEB";
-
-    const colorGraph=(num)=>{
-        const intervalSize = 10;
-        const index = Math.floor(num / intervalSize);
-        const decimalPart = (num % intervalSize) / intervalSize;
-        return index + decimalPart +1;
-      }
-
-  
-    return (
-      <>
-        <span style={{ color: props.color, fontWeight: "bold" }}>{props.value}%
-        </span>
-        {divArray.map((item, index) => (
-          <div className="bar" key={index}>
+export const StackedGraph = (props) => {
+  const white = "#EBEBEB";
+  const colorGraph = (num) => {
+    const intervalSize = 10;
+    const index = Math.floor(num / intervalSize);
+    const decimalPart = (num % intervalSize) / intervalSize;
+    return index + decimalPart + 1;
+  }
+  return (
+    <Fragment>
+      <span style={{ color: props.color, fontWeight: "bold" }}>{props.value}%</span>
+      {(() => {
+        const elements = [];
+        for (let index = 10; index > 0; index--) {
+          elements.push(<div className="bar" key={index}>
             <span
               className="barGraphRight"
               style={{
                 backgroundColor:
-                (item < colorGraph(props.value))? props.color : white,
-              }}
-            ></span>
+                  (index < colorGraph(props.value)) ? props.color : white,
+              }}>
+            </span>
             <span
               className="barGraphLeft"
               style={{
                 backgroundColor:
-                ((item + 1) <= colorGraph(props.value)) ? props.color : white,
-              
-              }}
-            ></span>
-          </div>
-        ))}
-      </>
-    );
-  }
-  
+                  ((index + 1) <= colorGraph(props.value)) ? props.color : white,
+              }}>
+            </span>
+          </div>);
+        }
+        return elements;
+      })()}
+    </Fragment>
+  );}
+export default StackedGraph
